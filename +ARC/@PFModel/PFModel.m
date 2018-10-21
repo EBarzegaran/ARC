a707343c862554fde02f75df466add32a2c2c2f4
EBarzegaran <e.barzegaran@gmail.com>
2018-10-21 00:51:21 -0700

    add peak and width

--- +ARC/@PFModel/PFModel.m
+++ +ARC/@PFModel/PFModel.m
@@ -9,6 +9,11 @@ classdef PFModel
         Conditions
         CondLength
     end
+    properties (Dependent)
+       ARpeaks
+       ARwidths
+       ARTempMeans
+    end
     
     methods
         function obj = PFModel(SubjectInfo, Model,Modes,Freq,Elabel,Conditions,CondLength)
@@ -56,5 +61,53 @@ classdef PFModel
             end
         end
         
+        function value = get.ARpeaks(obj)
+            % find the Modes which is frequency
+            M = find(strcmpi(obj.Modes,'frequency'));
+            % Then find their peak... if it is minimum or maximum?
+            if ~isempty(M)
+                Fmode = obj.Model{M};
+                [~,Ind] = max(Fmode);
+                value = obj.Freq(Ind);
+            else
+                value = [];
+            end
+        end
+        
+        function value = get.ARwidths(obj)
+            % find the Modes which is frequency
+            M = find(strcmpi(obj.Modes,'frequency'));
+            % Then find full width half maximym...
+            if ~isempty(M)
+                Fmode = obj.Model{M};
+                [M,MI] = max(Fmode);
+                [m] = mean(Fmode); % OR mean
+                HM= (M+m)/2; % half maximum
+                for c = 1:numel(HM)
+                    ind1 = diff(Fmode(:,c)<HM(c)); 
+                    I1=find(ind1==-1)'; I2=find(ind1==1)';
+                    if numel(I1)==numel(I2)
+                        I = [I1;I2];
+                        % fin the peak with global maximum
+                        Ind = MI(c)>=I(1,:).*MI(c)<=I(2,:);
+                        Width(c) = (I2(Ind)-I1(Ind)).*diff(obj.Freq(1:2));
+                    else
+                        Peak(c) = 1;
+                        Width(c) = 0;
+                    end 
+                end
+                value = Width;
+            else
+                value = [];
+            end
+        end
+        
+        function value = get.ARTempMeans(obj)
+            TempLen = [0 cumsum(obj.CondLength)];
+            Rang = [TempLen(1:end-1)+1;TempLen(2:end)];
+            Temp= obj.Model{(strcmpi(obj.Modes,'temporal'))};
+            TempM = arrayfun(@(x) mean(Temp(Rang(1,x):Rang(2,x),:)),1:size(Rang,2),'uni',false);
+            value = cat(1,TempM{:});
+        end
     end
 end
